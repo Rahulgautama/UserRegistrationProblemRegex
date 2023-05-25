a87d0b3dde6e2a4ad6bf8824ba651395fa99d0a7
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 01:31:42 +0530

    User Password Must have one number validate

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -8,9 +8,9 @@ namespace UserRegistrationProblemRegex
         static void Main(string[] args)
         {
            
-                Console.WriteLine("Please Enter your Password min 8 digit upper,lower,number,special sym");
+                Console.WriteLine("Please Enter your Password min 8 digit upper,lower,number");
                 var lastName = Console.ReadLine();
-                var regexDt = new Regex(@"^[a-zA-Z]{8,16}$");
+                var regexDt = new Regex(@"^\w{8,16}$");
 
                 if (regexDt.IsMatch(lastName))
                     Console.WriteLine("Valid Password ");
