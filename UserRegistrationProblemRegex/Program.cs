7da2a20b25c76daccc8e04d86ab4a8e10e821bef
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 01:06:42 +0530

    User Mobile Number validate

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -8,14 +8,14 @@ namespace UserRegistrationProblemRegex
         static void Main(string[] args)
         {
            
-                Console.WriteLine("Please Enter your Email");
+                Console.WriteLine("Please Enter your Mobile Number");
                 var lastName = Console.ReadLine();
-                var regexDt = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                var regexDt = new Regex(@"^(\+[0-9]{12,13})$");
 
                 if (regexDt.IsMatch(lastName))
-                    Console.WriteLine("Valid Email Name");
+                    Console.WriteLine("Valid Mobile Number");
                 else
-                    Console.WriteLine("Not Valid Email Name");
+                    Console.WriteLine("Not Valid Mobile number");
 
             
             
