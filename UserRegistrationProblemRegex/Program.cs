5a4c971c6fcd5ae56d050fed2d48f9cf254ad5a3
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 01:23:56 +0530

    User Password validate

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -10,7 +10,7 @@ namespace UserRegistrationProblemRegex
            
                 Console.WriteLine("Please Enter your Password min 8 digit upper,lower,number,special sym");
                 var lastName = Console.ReadLine();
-                var regexDt = new Regex(@"(?=^.{8,16}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+                var regexDt = new Regex(@"^\w{8,16}$");
 
                 if (regexDt.IsMatch(lastName))
                     Console.WriteLine("Valid Password ");
