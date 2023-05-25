06eac8e769e7f441bc231bf367c0b2e84d5a0082
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 01:39:50 +0530

    User Password Must have one special char validate

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -8,9 +8,9 @@ namespace UserRegistrationProblemRegex
         static void Main(string[] args)
         {
            
-                Console.WriteLine("Please Enter your Password min 8 digit upper,lower,number");
+                Console.WriteLine("Please Enter your Password min 8 digit upper,lower,number,special char");
                 var lastName = Console.ReadLine();
-                var regexDt = new Regex(@"^\w{8,16}$");
+                var regexDt = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
 
                 if (regexDt.IsMatch(lastName))
                     Console.WriteLine("Valid Password ");
